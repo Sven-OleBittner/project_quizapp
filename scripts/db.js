68baa2449d34b0327aaafb4f8a67dd5f14dbe05c
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-02-26 08:44:57 +0100

    validate the JSON

--- scripts/db.js
+++ scripts/db.js
@@ -5,15 +5,15 @@ let questions = [
         "anwer_2" : "Lady Gaga",
         "anwer_3" : "Tim Bernes-Lee",
         "anwer_4" : "Justin Bieber",
-        "right_answer" : 3,
+        "right_answer" : 3
     },
     {
         "question" : "Zwischen welchen tags bindet man JavaScript ein?",
         "answer_1" : "<javascript> und </javascript>",
         "answer_2" : "<script> und </script>",
-        "answer_3" : "<head></head>",
-        "answer_4" : "<html></html>",
-        "right_answer" : 2,
+        "answer_3" : "<head> und </head>",
+        "answer_4" : "<html> und </html>",
+        "right_answer" : 2
     },
     {
         "question" : "Was ist die korekte Syntax um eine JavaScript Datei in ein HTML Dokument einzubinden?",
@@ -21,7 +21,7 @@ let questions = [
         "answer_2" : "<script type='myScript.js'></script>",
         "answer_3" : "<script>myScript.js</script>",
         "answer_4" : "<script src='myScript.js'></script>",
-        "right_answer" : 4,
+        "right_answer" : 4
     },
     {
         "question" : "Welcher CSS Selektor wird benutzt um ein Element mit einer Spezifischen ID anzusprechen?",
@@ -29,7 +29,7 @@ let questions = [
         "answer_2" : ".",
         "answer_3" : "#",
         "answer_4" : "%",
-        "right_answer" : 3,
+        "right_answer" : 3
     },
     {
         "question" : "Für was steht HTML?",
@@ -37,6 +37,6 @@ let questions = [
         "answer_2" : "Home Typing Modern Language",
         "answer_3" : "Hyper Text Markup Language",
         "answer_4" : "Home Testing Mixed Language",
-        "right_answer" : 3,
-    },
+        "right_answer" : 3
+    }
 ];
