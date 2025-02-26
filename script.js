652e0c31755076bfe503555d52188e3f737be390
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-02-26 08:54:29 +0100

    started with the init() funktion

---
+++ script.js
@@ -0,0 +1,5 @@
+function init() {
+    let questionsFullNumRef = document.getElementById("questionsFullNum");
+    questionsFullNumRef.innerHTML = "";
+    questionsFullNumRef.innerHTML = questions.length;
+}
